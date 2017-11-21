3e42164c0ee9e4b8942f908f8e0744cb6ea58e06
Tiago Schenkel <tiago.schenkel@gmail.com>
2017-11-21 20:29:34 +0100

    add unit tests for StateManagerEvent class

---
+++ test/robotlegs/bender/extensions/stateManager/support/CallbackStateHandler.ts
@@ -0,0 +1,27 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017 RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
+import { IClass } from "@robotlegsjs/core";
+
+import { IStateHandler } from "../../../../../../src/robotlegs/bender/extensions/stateManager/api/IStateHandler";
+
+/**
+ * @private
+ */
+export class CallbackStateHandler implements IStateHandler {
+    private _callback: Function;
+
+    constructor(callback: Function = null) {
+        this._callback = callback;
+    }
+
+    public handleState(state: Phaser.State, type: IClass<any>): void {
+        if (this._callback) {
+            this._callback(state, type);
+        }
+    }
+}
