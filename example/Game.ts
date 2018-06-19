77db5a414c6c5bc2d249d89d6442336045226f55
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-06-19 14:06:56 +0200

    setup example

---
+++ example/Game.ts
@@ -0,0 +1,47 @@
+import { Context, IContext, MVCSBundle } from "@robotlegsjs/core";
+import { PhaserBundle } from "../src/robotlegs/bender/bundles/phaser/PhaserBundle";
+import { ContextStateManager } from "../src/robotlegs/bender/extensions/contextStateManager/impl/ContextStateManager";
+
+import { StateKey } from "./constants/StateKey";
+
+import { Boot } from "./states/Boot";
+import { Preload } from "./states/Preload";
+import { GameTitle } from "./states/GameTitle";
+import { Main } from "./states/Main";
+import { GameOver } from "./states/GameOver";
+
+import { GameConfig } from "./config/GameConfig";
+import { StateMediatorConfig } from "./config/StateMediatorConfig";
+
+export class Game extends Phaser.Game {
+
+    private _context: IContext;
+
+    constructor(
+        width?: number | string,
+        height?: number | string,
+        renderer?: number,
+        parent?: any,
+        state?: any,
+        transparent?: boolean,
+        antialias?: boolean,
+        physicsConfig?: any
+    ) {
+        super(width, height, renderer, parent, state, transparent, antialias, physicsConfig);
+
+        this._context = new Context();
+        this._context.install(MVCSBundle, PhaserBundle)
+            .configure(new ContextStateManager(this.state))
+            .configure(StateMediatorConfig)
+            .configure(GameConfig)
+            .initialize();
+
+        this.state.add(StateKey.BOOT, Boot, false);
+        this.state.add(StateKey.PRELOAD, Preload, false);
+        this.state.add(StateKey.GAME_TITLE, GameTitle, false);
+        this.state.add(StateKey.MAIN, Main, false);
+        this.state.add(StateKey.GAME_OVER, GameOver, false);
+
+        this.state.start(StateKey.BOOT);
+    }
+}
--- example/index.ts
+++ example/index.ts
@@ -2,44 +2,13 @@
 
 import "reflect-metadata";
 
-import { Context, IContext, MVCSBundle } from "@robotlegsjs/core";
-import { PhaserBundle } from "../src/robotlegs/bender/bundles/phaser/PhaserBundle";
-import { ContextStateManager } from "../src/robotlegs/bender/extensions/contextStateManager/impl/ContextStateManager";
+import { Game } from "./Game";
 
-import { StateKey } from "./constants/StateKey";
-
-import { Boot } from "./states/Boot";
-import { Preload } from "./states/Preload";
-import { GameTitle } from "./states/GameTitle";
-import { Main } from "./states/Main";
-import { GameOver } from "./states/GameOver";
-
-import { GameConfig } from "./config/GameConfig";
-import { StateMediatorConfig } from "./config/StateMediatorConfig";
-
-class Game extends Phaser.Game {
-
-    private _context: IContext;
-
-    constructor() {
-
-        super(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO);
-
-        this._context = new Context();
-        this._context.install(MVCSBundle, PhaserBundle)
-            .configure(new ContextStateManager(this.state))
-            .configure(StateMediatorConfig)
-            .configure(GameConfig)
-            .initialize();
-
-        this.state.add(StateKey.BOOT, Boot, false);
-        this.state.add(StateKey.PRELOAD, Preload, false);
-        this.state.add(StateKey.GAME_TITLE, GameTitle, false);
-        this.state.add(StateKey.MAIN, Main, false);
-        this.state.add(StateKey.GAME_OVER, GameOver, false);
-
-        this.state.start(StateKey.BOOT);
-    }
-}
-
-new Game();
+(<any>window).initGame = function() {
+    let game: Game = new Game(
+        window.innerWidth * window.devicePixelRatio,
+        window.innerHeight * window.devicePixelRatio,
+        Phaser.AUTO
+    );
+    (<any>window).game = game;
+};
