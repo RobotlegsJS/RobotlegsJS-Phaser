7296ed3c3a6b4479946777f6f678af936e91aa48
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-07-30 21:15:18 +0200

    adopts year-agnostic copyright message

--- example/Game.ts
+++ example/Game.ts
@@ -1,3 +1,10 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017-present, RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
 import { Context, IContext, MVCSBundle } from "@robotlegsjs/core";
 import { PhaserBundle } from "../src/robotlegs/bender/bundles/phaser/PhaserBundle";
 import { ContextStateManager } from "../src/robotlegs/bender/extensions/contextStateManager/impl/ContextStateManager";
--- test/entry.ts
+++ test/entry.ts
@@ -1,3 +1,10 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017-present, RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
 /// <reference path="../node_modules/phaser-ce/typescript/phaser.d.ts" />
 
 import "reflect-metadata";
