d0d6ddd7b064a5b67411c0ff751b5a44e6810573
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-08-06 12:16:30 +0200

    update webpack config for example

--- webpack.example.config.js
+++ webpack.example.config.js
@@ -4,52 +4,59 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ConcatPlugin = require("webpack-concat-plugin");
 
 const concatPluginConfigGenerator = (name, files) => {
-    return {
-        uglify: false,
-        sourceMap: false,
-        name: name,
-        fileName: "[name].js",
-        filesToConcat: files,
-        injectType: "none"
-    };
+  return {
+    uglify: false,
+    sourceMap: false,
+    name: name,
+    fileName: "[name].js",
+    filesToConcat: files,
+    injectType: "none"
+  };
 };
 
 module.exports = options => {
-    return {
-        mode: "development",
+  return {
+    mode: "development",
 
-        entry: {
-            main: path.resolve("example/index.ts")
-        },
+    entry: {
+      main: path.resolve("example/index.ts")
+    },
 
-        output: {
-            path: __dirname + "/dist",
-            filename: "bundle.js"
-        },
+    output: {
+      path: __dirname + "/dist",
+      filename: "bundle.js"
+    },
 
-        devtool: "source-map",
+    devtool: 'source-map',
 
-        module: {
-            rules: [{ test: /\.ts$/, loader: "ts-loader" }]
-        },
+    module: {
+      rules: [
+        { test: /\.ts$/, loader: "ts-loader" }
+      ]
+    },
 
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve("static/index.html"),
-                inject: false
-            })
-        ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve("static/index.html"),
+        inject: false
+      }),
 
-        resolve: {
-            extensions: [".ts", ".js", ".json"]
-        },
+      new ConcatPlugin(concatPluginConfigGenerator("phaser", [
+        path.resolve(__dirname, "./node_modules/phaser/dist/phaser.js")
+      ])),
+    ],
 
-        devServer: {
-            host: "0.0.0.0",
-            contentBase: path.join(__dirname, "static"),
-            hot: true,
-            disableHostCheck: true,
-            inline: false
-        }
-    };
+    resolve: {
+      extensions: ['.ts', '.js', '.json']
+    },
+
+    devServer: {
+      host: "0.0.0.0",
+      contentBase: path.join(__dirname, "static"),
+      hot: true,
+      disableHostCheck: true,
+      inline:false
+    }
+
+  }
 };
