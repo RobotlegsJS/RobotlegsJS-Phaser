f387a4db8e95126c3b381a42e4b2489d1b803db4
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-08-06 11:19:52 +0200

    update copyright message

--- test/entry.ts
+++ test/entry.ts
@@ -1,3 +1,10 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) 2017-present, RobotlegsJS. All Rights Reserved.
+//
+//  NOTICE: You are permitted to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+// ------------------------------------------------------------------------------
+
 /// <reference types="mocha" />
 /// <reference path="../definitions/phaser.d.ts" />
 import "bluebird/js/browser/bluebird";
--- test/robotlegs/bender/bundles/phaser/phaserBundle.test.ts
+++ test/robotlegs/bender/bundles/phaser/phaserBundle.test.ts
@@ -1,9 +1,10 @@
 // ------------------------------------------------------------------------------
-//  Copyright (c) 2017 RobotlegsJS. All Rights Reserved.
+//  Copyright (c) 2017-present, RobotlegsJS. All Rights Reserved.
 //
 //  NOTICE: You are permitted to use, modify, and distribute this file
 //  in accordance with the terms of the license agreement accompanying it.
 // ------------------------------------------------------------------------------
+
 import "../../../../entry";
 import { Context, IContext } from "@robotlegsjs/core";
 import { assert } from "chai";
