f60c464d4927604361fffd03921997d33a2a6ca8
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-08-06 12:11:30 +0200

    rearranges export statements

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,27 @@
 // ------------------------------------------------------------------------------
 
 // ContextSceneManager
-/**
- * Bundles
- */
-/// <reference path="../definitions/phaser.d.ts" />
-export { PhaserBundle } from "./robotlegs/bender/bundles/phaser/PhaserBundle";
 export { IContextSceneManager } from "./robotlegs/bender/extensions/contextSceneManager/api/IContextSceneManager";
-export { ContextSceneManagerExtension } from "./robotlegs/bender/extensions/contextSceneManager/ContextSceneManagerExtension";
 export { ContextSceneManager } from "./robotlegs/bender/extensions/contextSceneManager/impl/ContextSceneManager";
 export {
     ContextSceneManagerListenerConfig
 } from "./robotlegs/bender/extensions/contextSceneManager/impl/ContextSceneManagerListenerConfig";
+export { ContextSceneManagerExtension } from "./robotlegs/bender/extensions/contextSceneManager/ContextSceneManagerExtension";
+
 // SceneManager
 export { ISceneHandler } from "./robotlegs/bender/extensions/sceneManager/api/ISceneHandler";
 export { ISceneManager } from "./robotlegs/bender/extensions/sceneManager/api/ISceneManager";
 export { SceneManagerExtension } from "./robotlegs/bender/extensions/sceneManager/SceneManagerExtension";
+export { SceneManagerObserverExtension } from "./robotlegs/bender/extensions/sceneManager/SceneManagerObserverExtension";
+
 // SceneMediatorMap
 export { ISceneMediator } from "./robotlegs/bender/extensions/sceneMediatorMap/api/ISceneMediator";
 export { ISceneMediatorMap } from "./robotlegs/bender/extensions/sceneMediatorMap/api/ISceneMediatorMap";
 export { ISceneMediatorMapping } from "./robotlegs/bender/extensions/sceneMediatorMap/api/ISceneMediatorMapping";
 export { SceneMediator } from "./robotlegs/bender/extensions/sceneMediatorMap/impl/SceneMediator";
 export { SceneMediatorMapExtension } from "./robotlegs/bender/extensions/sceneMediatorMap/SceneMediatorMapExtension";
+
+/**
+ * Bundles
+ */
+export { PhaserBundle } from "./robotlegs/bender/bundles/phaser/PhaserBundle";
