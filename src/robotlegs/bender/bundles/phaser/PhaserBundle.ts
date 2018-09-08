ee247b94a811450564966db228bc27e2c52f0411
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-09-08 14:36:35 +0200

    PhaserBundle logs an error message when IContextSceneManager is not installed

--- src/robotlegs/bender/bundles/phaser/PhaserBundle.ts
+++ src/robotlegs/bender/bundles/phaser/PhaserBundle.ts
@@ -5,14 +5,18 @@
 //  in accordance with the terms of the license agreement accompanying it.
 // ------------------------------------------------------------------------------
 
-import { IBundle, IContext } from "@robotlegsjs/core";
+import { IBundle, IContext, ILogger, instanceOfType } from "@robotlegsjs/core";
 
-import { ContextSceneManagerExtension } from "../../extensions/contextSceneManager/ContextSceneManagerExtension";
-import { SceneManagerExtension } from "../../extensions/viewManager/SceneManagerExtension";
-import { SceneMediatorMapExtension } from "../../extensions/mediatorMap/SceneMediatorMapExtension";
+import { IContextSceneManager } from "../../extensions/contextSceneManager/api/IContextSceneManager";
+import { ContextSceneManager } from "../../extensions/contextSceneManager/impl/ContextSceneManager";
 import { ContextSceneManagerListenerConfig } from "../../extensions/contextSceneManager/impl/ContextSceneManagerListenerConfig";
-import { SceneManagerObserverExtension } from "../../extensions/viewManager/SceneManagerObserverExtension";
+import { ContextSceneManagerExtension } from "../../extensions/contextSceneManager/ContextSceneManagerExtension";
+
+import { SceneMediatorMapExtension } from "../../extensions/mediatorMap/SceneMediatorMapExtension";
 import { ViewMediatorMapExtension } from "../../extensions/mediatorMap/ViewMediatorMapExtension";
+
+import { SceneManagerExtension } from "../../extensions/viewManager/SceneManagerExtension";
+import { SceneManagerObserverExtension } from "../../extensions/viewManager/SceneManagerObserverExtension";
 
 /**
  * For that Classic Robotlegs flavour
@@ -22,6 +26,13 @@ import { ViewMediatorMapExtension } from "../../extensions/mediatorMap/ViewMedia
  */
 export class PhaserBundle implements IBundle {
     /*============================================================================*/
+    /* Private Properties                                                         */
+    /*============================================================================*/
+
+    private _context: IContext;
+    private _logger: ILogger;
+
+    /*============================================================================*/
     /* Public Functions                                                           */
     /*============================================================================*/
 
@@ -29,7 +40,10 @@ export class PhaserBundle implements IBundle {
      * @inheritDoc
      */
     public extend(context: IContext): void {
-        context.install(
+        this._context = context;
+        this._logger = context.getLogger(this);
+
+        this._context.install(
             ContextSceneManagerExtension,
             SceneManagerExtension,
             SceneManagerObserverExtension,
@@ -37,6 +51,27 @@ export class PhaserBundle implements IBundle {
             ViewMediatorMapExtension
         );
 
-        context.configure(ContextSceneManagerListenerConfig);
+        this._context.addConfigHandler(instanceOfType(ContextSceneManager), this.handleContextSceneManager.bind(this));
+        this._context.whenInitializing(this.whenInitializing.bind(this));
+        this._context.afterDestroying(this.afterDestroying.bind(this));
+    }
+
+    /*============================================================================*/
+    /* Private Functions                                                          */
+    /*============================================================================*/
+
+    private handleContextSceneManager(contextSceneManager: ContextSceneManager): void {
+        this._context.configure(ContextSceneManagerListenerConfig);
+    }
+
+    private whenInitializing(): void {
+        if (!this._context.injector.isBound(IContextSceneManager)) {
+            this._logger.error("PhaserBundle requires IContextSceneManager.");
+        }
+    }
+
+    private afterDestroying(): void {
+        this._context = null;
+        this._logger = null;
     }
 }
