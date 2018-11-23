58911b4c9f26a6d39b8faaa238a6c82cc3ce5497
Tiago Schenkel <tiago.schenkel@gmail.com>
2018-11-23 15:24:23 +0100

    improve performance of karma

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ const webpackConfig = require("./webpack.config.js")({ production: false, karma:
 delete webpackConfig.entry;
 
 module.exports = config => {
+  "use strict";
 
   var configuration = {
     client: {
@@ -19,20 +20,19 @@ module.exports = config => {
     basePath: "",
     frameworks: [
       "mocha",
-      "chai",
-      "sinon",
+      "sinon-chai",
       "es6-shim"
     ],
     files: [
       { pattern: "node_modules/reflect-metadata/Reflect.js", include: true },
       { pattern: "node_modules/bluebird/js/browser/bluebird.js", include: true },
       { pattern: "node_modules/phaser/dist/phaser.js", include: true },
-      { pattern: "./test/**/**/**.test.ts", include: true },
+      { pattern: "./test/index.ts", include: true },
       { pattern: '**/*.map', served: true, included: false, watched: true }
     ],
     preprocessors: {
-      "./**/**/**/**.ts": ["sourcemap"],
-      "./test/**/**/**.test.ts": ["webpack"]
+      "./test/index.ts": ["webpack"],
+      "./**/**/**/**.ts": ["sourcemap"]
     },
     webpack: webpackConfig,
     webpackMiddleware: {
@@ -45,11 +45,13 @@ module.exports = config => {
       "karma-remap-istanbul",
       "karma-mocha-reporter",
       "karma-mocha",
-      "karma-chai",
-      "karma-sinon",
+      "karma-sinon-chai",
       "karma-es6-shim",
       "karma-coverage-istanbul-reporter"
     ],
+    mime: {
+      "text/x-typescript": ["ts","tsx"]
+    },
     reporters: (
       config.singleRun ?
         ["dots", "mocha", "coverage-istanbul"] :
